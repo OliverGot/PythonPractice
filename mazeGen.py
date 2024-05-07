b4e3f54a163f57ae04ab3edebb3998cb3290258c
Oliver Gotzinger <oliver.got@icloud.com>
2024-05-07 11:48:48 +1000

    test delete

--- mazeGen.py
+++ mazeGen.py
@@ -5,9 +5,3 @@ RES = WIDTH, HEIGHT = 1202 902
 TILE = 100
 cols, rows = WIDTH // TILE, HEIGHT // TILE
 
-pygame.init()
-sc = pygame.display.set_mode(RES)
-clock = pygame.time.Clock()
-
-while True:
-    sc.fill()
